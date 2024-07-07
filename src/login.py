74d752d27bd50cb4430a86434e3c1631d5a72dae
Talha Abdurehman <talhakyle124@gmail.com>
2024-07-06 17:09:55 -0700

    Set up the Menu and made it more intuitive

--- src/login.py
+++ src/login.py
@@ -1,6 +1,5 @@
 import sqlite3
 
-
 class LRegister:
     
     @classmethod
@@ -31,11 +30,42 @@ class LRegister:
                             print("Your credentials Match! You can successfully Login")
             else:
                 print("Account Not found!")
+ 
+ 
+ 
+    @classmethod
+    def mem_signup(cls,first:str, last:str, phone:int, password:str):
+        conn = sqlite3.connect("db/members.db")
+        cursor = conn.cursor()
+        
+        cursor.execute(f"INSERT INTO member (first_name, last_name, phone, password) VALUES ('{first}', '{last}', '{phone}', '{password}' )")
+        
+        conn.commit()
+
+            
+    @classmethod
+    def mem_login(cls,first:str, last:str, phone:int, password:str):
+        conn = sqlite3.connect("db/members.db")
+        cursor = conn.cursor()
+        
+        cursor.execute(f"SELECT * FROM member WHERE first_name = '{first}' AND last_name = '{last}' AND phone = '{phone}' AND password = '{password}' ")
+        data = cursor.fetchall()
+        
+        conn.commit()
+        
+        for d in data:
+            if first == d[1] \
+                and last == d[2] \
+                    and phone == d[3]\
+                        and password == d[4]:
+                            print("Your credentials Match! You can successfully Login")
+            else:
+                print("Account Not found!")
 
 
 
 
 if __name__ == "__main__":
     pass
-        
+    
          
